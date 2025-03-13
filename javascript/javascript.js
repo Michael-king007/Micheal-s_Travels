01d2316155624eb8677ea94530543e9b3cc378ac
Michael-king007 <michelecancedda0710@gmail.com>
2025-03-13 13:32:59 +0100

    Create javascript.js

---
+++ javascript/javascript.js
@@ -0,0 +1,20 @@
+function bottone()
+{
+    alert("NUOVO UTENTE REGISTRATO") ;
+}
+function accedi()
+{
+    alert("ACCESSO EFFETTUATO CORRETTAMENTE");
+}
+function acquisto()
+{
+    alert("ACQUISTO EFFETTUATO CORRETTAMENTAMENTE");
+}
+function struttura()
+{
+    alert("STRUTTURA REGISTRATA CORRETTAMENTE");
+}
+function reset()
+{
+    ;
+}
